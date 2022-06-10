5b83bc95aa40f09232e4a2779d3153f5570c3a31
Prateek Varma <prtk.varma@gmail.com>
2022-06-10 15:31:56 -0600

    used axios object instance 'youtube' to build the final get query (not finished yet)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  //The 'term' does not require require props because it is passed up from child to parent.
   onTermSubmit = (term) => {
-    //below there is no need to use props to access the 'term' because it is passed up from the child to parent.
-    console.log(term)
+    //Below, 'youtube' is an instance of axios, so we can perform a get query on it. Moreover, we have appended 'search' and a few more params here, which add on to the previous ones defined in youtube.js
+    youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
   };
 
   render() {
