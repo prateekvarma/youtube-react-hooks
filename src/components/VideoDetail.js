df85a64fa9dbe7f8cc29e6abcaaf206b3cbe2f3b
Prateek Varma <prtk.varma@gmail.com>
2022-06-15 14:45:53 -0600

    displayed YouTube video player using an iframe, and styled with some css

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 const VideoDetail = (props) => {
-    // Below, the "if" condition is just temporary, to overlook the null value of props.video
-    if(!props.video){
-        return <div>Loading...</div>
-    }
-    return <div>{props.video.snippet.title}</div>
+  // Below, the "if" condition is just temporary, to overlook the null value of props.video
+  if (!props.video) {
+    return <div>Loading...</div>;
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} title="YouTube video player" />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{props.video.snippet.title}</h4>
+        <p>{props.video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
