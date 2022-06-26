24674464c146eb0e16be6ab553719ea5e03abeda
Prateek Varma <prtk.varma@gmail.com>
2022-06-26 12:37:31 -0600

    Refactored the SearchBar component from class to functional

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  //Below, is an 'event callback' function, so we're defining it as an arrow function , to perform an callback binding.
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
   };
 
-  //Below, this name should match the prop in the parent component. So, we're sending data from child to parent.
-  onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    
-    //below, we're using props to access the 'onFormSubmit' function from the parent. So data os flowing from parent to child. And then finally using 'this.state.term' as parameters to pass data from child to parent.
-    this.props.onFormSubmit(this.state.term)
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={onInputChange} />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
